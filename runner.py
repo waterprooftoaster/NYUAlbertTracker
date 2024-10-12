6c844550b5a9915e2f34fba182e108252c51bf2c
waterprooftoaster <awu200592@gmail.com>
2024-10-11 21:53:25 -0400

    imported selenium

--- runner.py
+++ runner.py
@@ -1,3 +1,12 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests 
 
-print ("Hello World")
+url = "https://albert.nyu.edu/albert_index.html"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html")
+print(soup)
+
+username = "cw4483"
+password = "password"
+driver = webdriver.Chrome()
